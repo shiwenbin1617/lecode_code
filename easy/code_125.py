587c246613adac8f4ab2c7a1bce07d4ec97636d2
shiwenbin <shiwenbin1617@gmail.com>
2023-06-18 15:28:55 +0800

    69,判断回文串，解题方法

---
+++ easy/code_125.py
@@ -0,0 +1,23 @@
+"""
+如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+
+字母和数字都属于字母数字字符。
+
+给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+
+"""
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c.lower() for c in s if c.isalnum())
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
