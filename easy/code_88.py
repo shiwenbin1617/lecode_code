d5c635f1a5b83f91209c1b36de465b2d2b38c882
shiwenbin <shiwenbin1617@gmail.com>
2023-06-18 15:14:50 +0800

    69,数组合并，解题方法

---
+++ easy/code_88.py
@@ -0,0 +1,24 @@
+"""
+给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+
+"""
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n == 0:
+            return nums1
+        k = 0
+        for i in range(len(nums1) - 1, -1, -1):
+            if nums1[i] == 0:
+                nums1.pop(i)
+                k += 1
+                if k == n:
+                    break
+        nums1.extend(nums2)
+        nums1.sort()
